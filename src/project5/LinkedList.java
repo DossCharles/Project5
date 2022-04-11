3661d0931a112a27018929167d7ed76c6db005e0
DossCharles <83914892+DossCharles@users.noreply.github.com>
2022-04-11 19:41:13 -0400

    Linkedlist

--- src/project5/LinkedList.java
+++ src/project5/LinkedList.java
@@ -1,5 +1,28 @@
 package project5;
 
+import java.util.Iterator;
+
 public class LinkedList<T> {
 
+    private class Node<T> {
+
+    }
+
+
+    private class LLIterator<T> implements Iterator<T> {
+
+        @Override
+        public boolean hasNext() {
+            // TODO Auto-generated method stub
+            return false;
+        }
+
+
+        @Override
+        public T next() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+    }
 }
